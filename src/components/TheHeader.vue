f987ab8e39d1283ed3af2134a3ee7539143e9320
Lucas5990 <lucasandrade5990@gmail.com>
2021-07-24 16:01:46 -0300

    feat: Add TheHeader component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <nav>
+      <img class="logo" src="@/assets/ranek.svg" alt="" />
+      <button
+        onclick="alert('Sinto muito, eu ainda não faço nada!')"
+        class="btn"
+      >
+        Vender / Login
+      </button>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "TheHeader",
+};
+</script>
+
+<style scoped>
+nav {
+  padding: 15px 20px;
+  display: flex;
+  justify-content: space-between;
+  box-shadow: 0 2px 4px rgba(30, 60, 90, 0.1);
+}
+.logo {
+  width: 90px;
+}
+</style>
